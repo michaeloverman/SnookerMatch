b6c40b997e7bf4997c155c83114b22d66bea5c45
michaeloverman <overman.michael@gmail.com>
2016-03-06 00:53:04 -0500

    click listeners working

--- app/src/main/java/com/overman/snookermatch/ScoringActivity.java
+++ app/src/main/java/com/overman/snookermatch/ScoringActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,6 +31,7 @@ public class ScoringActivity extends AppCompatActivity {
     @Bind(R.id.blueBall) ImageView mBlueBall;
     @Bind(R.id.pinkBall) ImageView mPinkBall;
     @Bind(R.id.blackBall) ImageView mBlackBall;
+    @Bind(R.id.ballsLayout) LinearLayout mBalls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +43,7 @@ public class ScoringActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         mPlayer1 = new Player(intent.getStringExtra("name1"));
-//        Log.d(TAG, "mPlayer1 name: " + mPlayer1.getName());
         mPlayer2 = new Player(intent.getStringExtra("name2"));
-//        Log.d(TAG, "mPlayer2 name: " + mPlayer2.getName());
 
         mMatch = new Match(mPlayer1, mPlayer2, intent.getIntExtra("frames", 1));
         Log.d(TAG, "Match created " + mMatch.getPlayer1().getName() + ", " + mMatch.getPlayer2().getName() + ", best of " + mMatch.getFrames());
@@ -51,5 +53,89 @@ public class ScoringActivity extends AppCompatActivity {
         mPlayer1Score.setText(mPlayer1.getScore() + "");
         mPlayer2Score.setText(mPlayer2.getScore() + "");
 
+        mRedBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("red");
+            }
+        });
+        mYellowBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("yellow");
+            }
+        });
+        mGreenBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("green");
+            }
+        });
+        mBrownBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("brown");
+            }
+        });
+        mBlueBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("blue");
+            }
+        });
+        mPinkBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("pink");
+            }
+        });
+        mBlackBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballPotted("black");
+            }
+        });
+        mPlayer1Label.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMe("player 1 clicked");
+            }
+        });
+        mPlayer2Label.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMe("player 2 clicked");
+            }
+        });
     }
+
+    private void ballPotted(String color) {
+        int score = 0;
+        switch (color) {
+            case "black":
+                score += 1;
+            case "pink":
+                score += 1;
+            case "blue":
+                score += 1;
+            case "brown":
+                score += 1;
+            case "green":
+                score += 1;
+            case "yellow":
+                score += 1;
+            case "red":
+                score += 1;
+        }
+        toastMe(color + " ball potted, " + score + " points");
+    }
+
+    /**
+     * A primarily useless Toast method - helpful for debugging purposes
+     * @param s
+     */
+    private void toastMe(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
 }
